8c4ef2218b6575fa4627bd7cb6970a01ae7fb180
Cinthia <49377882+CINPIS@users.noreply.github.com>
2020-08-05 17:42:51 -0300

    Update server.js

--- ecoleta/src/server.js
+++ ecoleta/src/server.js
@@ -1,13 +1,10 @@
 const express = require("express")
 const server = express()
 
-//pegar banco de dados
 const db = require("./database/db.js")
 
-//public folder access
 server.use(express.static("public"))
 
-//habilitar uso do req.body. USE para configurar.
 server.use(express.urlencoded({ extended: true }))
 
 server.get("/", (req, res) => {
@@ -16,27 +13,14 @@ server.get("/", (req, res) => {
     })
 })
 
-//com o formulario preenchido, precisamos
-//de requisição tbm, atributo req.query, pois
-//estamos pegando a query string ou link enviado pelo front
-//form
 server.get("/create-point", (req, res) => {
     
-    //console.log(req.query)
-    //req.query
-
     return res.render("create-point.html")
-    // TESTE return res.render("create-point.html", { saved: true })
 
 })
-//outra rota para ele
-//no navegador o verbo é get. 
-//o post (lá no form: action="/save-point" method="POST")
+
 server.post("/save-point", (req, res) => {
     
-    //corpo do form. Mas tem que habilitar 
-    //console.log(req.body)
-    //agora tem que inserir os dados do form no bd
     const query = `
         INSERT INTO places (
             image,
@@ -66,7 +50,6 @@ server.post("/save-point", (req, res) => {
         console.log("cadastro realizado com sucesso!")
         console.log(this)
 
-        //return res.send("OK! Formulário enviado!")
         return res.render("create-point.html", { saved: true })
     }
     
@@ -79,11 +62,8 @@ server.get("/search", (req, res) => {
     const search = req.query.search
     if( search == "" ){
         return res.render("search-points.html", {total:0})
-        //mostra a pg html com os do banco total vazio = 0 ou nada
     }
 
-    //pegar dados do bd - template literals ou strings aceita 
-    //a ${} entre aspas simples. Like e % para aprox antes e depois.
     db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function(error, rows){
         if(error){
             return console.log(error)
@@ -91,11 +71,7 @@ server.get("/search", (req, res) => {
 
         const total = rows.length
 
-        //console.log("aqui estão os registros.")
-        //console.log(rows)
         return res.render("search-points.html", {places: rows, total:total})
-        //mostra a pg html com os do banco
-
     })
 
 })
@@ -107,7 +83,5 @@ nunjuckes.configure("src/views", {
     noCache: true
 })
 
-
-//ligar o servidor, ouça a porta 3000
 server.listen(3000)
 
