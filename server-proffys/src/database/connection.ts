9b14516f08699f3d1e8c7692b7e77a6193a9cae4
Cinthia <49377882+CINPIS@users.noreply.github.com>
2020-08-05 17:38:26 -0300

    Update connection.ts

--- server-proffys/src/database/connection.ts
+++ server-proffys/src/database/connection.ts
@@ -7,17 +7,7 @@ const db = knex({
     connection: {
         filename: path.resolve(__dirname, 'database.sqlite')
     },
-    useNullAsDefault: true, // por padrão sql lite não sabe preencher algo vazio, então atribuimos, nesse caso, o null
+    useNullAsDefault: true, 
 });
 
 export default db;
-
-// nisso se popula e cria as tabelas do banco,
-// no caso, vamos usar conceito de histórico, ou seja, migration,
-// para controlar a versão do banco e evitar duplicidades em caso
-// de mais de um programador trabalhar no mesmo código.
-// migrations serão executados e vão ver as atualizações.
-// knex tem uma cli... executando na linha de comando...
-// antes, ele executa com JavaScript, não TypeScript. Por isso
-// vamos criar na raiz do projeto um knexfile.ts.
-// dirname passa a raiz da aplicação, o diretorio,
