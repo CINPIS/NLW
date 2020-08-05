4ce1c0dbb5f5139ec64341d0a7e3ce83a415eb03
Cinthia <49377882+CINPIS@users.noreply.github.com>
2020-08-05 17:38:42 -0300

    Update convertHourToMinutes.ts

--- server-proffys/src/database/utils/convertHourToMinutes.ts
+++ server-proffys/src/database/utils/convertHourToMinutes.ts
@@ -1,5 +1,4 @@
 export default function convertHourToMinutes(time: string){
-    // hora e minutes 8:00
 
     const [hour, minutes] = time.split(':').map(Number)
     const timeInMinutes = (hour * 60) + minutes;
